0274fa8b69fb411b51a96e41ccdfe30f7547bf18
sabnadk <2141720009@student.polinema.ac.id>
2023-06-07 11:47:34 +0700

    Sabnaaa

---
+++ database/migrations/2023_06_07_042450_create_pegawai_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->integer ('id_pegawai');
+            $table->string('nama_pegawai', 25);
+            $table->string('jabatan', 25);
+            $table->integer('id_usaha');
+            $table->string('jenis_kelamin', 15);
+            $table->string('no_telp', 15);
+            $table->string('email', 25);
+            $table->string('password', 15);
+            $table->integer('id_alamat_pegawai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawai');
+    }
+};
