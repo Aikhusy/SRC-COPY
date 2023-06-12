24f6aa68a7251055f07d69757f17296659f0925c
MuhammadRifatA <mrifatanwar36@gmail.com>
2023-06-12 20:44:56 +0700

    tampilan login

--- resources/views/login/loginPage.blade.php
+++ resources/views/login/loginPage.blade.php
@@ -1,16 +1,43 @@
-<form action="{{ route('pengguna.login') }}" method="POST">
-    @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-    <label for="username">Username</label>
-    <input type="text" id="username" name="username">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<div>
+    <div class="container">
+        <div class="login">
+            <form action="">
+                <h1>Login</h1>
+                <hr>
+                <p>Toko Madura</p>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
 
-    <br><br>
+                <br><br>
 
-    <label for="password">Password</label>
-    <input type="text" id="Password" name="password">
+                <label for="password">Password</label>
+                <input type="text" id="Password" name="password">
 
-    <br><br>
+                <br><br>
 
-    <input type="submit" value="Submit">
-</form>
-<a href="{{ route('pengguna.create') }}">Belum Punya Akun?</a>
+                <button type="submit" value="Submit">Klik</button>
+                <p>
+                    <a href="{{ route('pengguna.create') }}">Belum Punya Akun?</a>
+                </p>
+            </form>
+        </div>
+        <div class="right">
+            <img src="{{ asset('images/gmbr1.png') }}" alt="">
+
+        </div>
+        <div>
+            <img src="" class="avatar">
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
